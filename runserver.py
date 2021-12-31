61e2e0e801f55beefc6a3929af54732cd06af550
HIDEN64 <dylanrick171@gmail.com>
2021-12-30 20:18:04 -0600

    only require ssl_context to be imported if HTTPS is enabled

--- runserver.py
+++ runserver.py
@@ -1,11 +1,17 @@
 from aiohttp import web
-from site_ctrl import run_site, ssl_context
+from site_ctrl import run_site
 
 import settings
 
 def main():
-	app = run_site(serve_static = True)
-	web.run_app(app, ssl_context=ssl_context, port = settings.PORT)
+	if settings.ENABLE_HTTPS == True:
+		from site_ctrl import ssl_context
+		app = run_site(serve_static = True)
+		web.run_app(app, ssl_context=ssl_context, port = settings.PORT)
+
+	else:
+		app = run_site(serve_static = True)
+		web.run_app(app, port = settings.PORT)
 
 if __name__ == '__main__':
 	main()
