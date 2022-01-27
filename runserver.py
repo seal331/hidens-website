5431f5529ae81cf096d7eed81ad74e6d5496d40b
HIDEN64 <hiden64@protonmail.org>
2022-01-27 10:13:21 -0600

    logging to file, systemd service, comment out unfinished pages

--- runserver.py
+++ runserver.py
@@ -11,8 +11,14 @@ def main(*, bool = False):
 		app = RunServ(serve_static = settings.SERVE_STATIC, ssl_context = TLSContext(settings.CERT_ROOT, settings.CERT_DIR).create_ssl_context(), serve_storage = settings.SERVE_STORAGE, serve_js = settings.SERVE_JS)
 
 	if settings.ENABLE_LOGGING:
-		logging.basicConfig(level=logging.DEBUG)
-		
+		logging.basicConfig (
+			filename='log.txt',
+            filemode='a',
+            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+            datefmt='%H:%M:%S',
+            level=logging.DEBUG
+		)
+				
 	web.run_app(app, port = settings.PORT)
 
 if __name__ == '__main__':
