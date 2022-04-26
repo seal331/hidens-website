435b4109c3341135360ddae7e248460a5466e753
HIDEN64 <hiden64@protonmail.com>
2022-04-25 20:05:53 -0500

    thangs

--- javascript/funfact.js
+++ javascript/funfact.js
@@ -8,8 +8,9 @@ function randInt(size) {
         Fact[1] = "This site was brought online on May 10th, 2021. It has greatly evolved since then.";
         Fact[2] = "This site was hosted on HM's secondary laptop until June 26th, 2021. It was hosted on GitHub pages until November 5th, 2021. Since then, it was self-hosted.";
         Fact[3] = "The best letter of the alphabet is objectively H."
+        Fact[4] = "This site had had a total of 3 - 5 extended downtimes in the ~1 year it's been active."
 
         return Fact[jNum];
     }
 
-document.write(getFact(randInt(3)));
+document.write(getFact(randInt(4)));
