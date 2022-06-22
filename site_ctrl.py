b95e942e786cb60aa893a1f9f80fd558c9a70075
HIDEN64 <hiden64@protonmail.com>
2022-06-22 02:57:40 -0500

    remove more unused code from site_ctrl

--- site_ctrl.py
+++ site_ctrl.py
@@ -21,8 +21,6 @@ def RunServ(serve_static = False, serve_storage = False, serve_js = False):
 	app.router.add_get('/downloads/cursors', page_downloads_cursors)
 	app.router.add_get('/about', page_about)
 	app.router.add_get('/about/contact', page_contact)
-	app.router.add_get('/about/basicinfo', page_basicinfo)
-	app.router.add_get('/about/favorite/software/games', page_favorite_games)
 	app.router.add_get('/computers', page_my_computers)
 	app.router.add_get('/mcsrv', page_mc_srv)
 	app.router.add_get('/mcsrv/rules', page_mc_srv_rules)
@@ -100,16 +98,6 @@ async def page_about(req):
 async def page_contact(req):
 	return render(req, 'about.contact.html', {
 		'title': 'Contact info | About me'
-	})
-
-async def page_basicinfo(req):
-	return render(req, 'about.basic.html', {
-		'title': 'Bio or something idk | About me'
-	})
-
-async def page_favorite_games(req):
-	return render(req, 'about.favorite.games.html', {
-		'title': 'Favorite games | About me'
 	})
 
 async def page_mc_srv(req):
