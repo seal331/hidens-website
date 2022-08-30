af0747771e6c210e64c913fb65e794b01c79b432
HIDEN64 <hiden64@protonmail.com>
2022-08-30 13:55:17 -0500

    stuff
    add a fun facts box, update some info, add some meta tags, change some text

--- javascript/funfact.js
+++ javascript/funfact.js
@@ -5,14 +5,15 @@ function randInt(size) {
 	function getFact(jNum) {
 		var Fact = new Array();
 
-		Fact[1] = "This site was brought online on May 10th, 2021. It has greatly evolved since then.";
-		Fact[2] = "This site was hosted on the secondary laptop of an old friend that I no longer talk to until June 26th, 2021. It was hosted on GitHub pages until November 5th, 2021. Since then, it was self-hosted.";
+		Fact[1] = "This site was brought online on May 10th, 2021. It has greatly evolved since then. <a class=stuffboxcontenttxt href=/static/unused/old/page/frontpage_index.html>You can view a glimpse of what it looked like then if you'd like</a>.";
+		Fact[2] = "This site was hosted on an ex-friend's spare laptop until June 26th, 2021. It was hosted on GitHub pages until November 5th, 2021. Since then, it was self-hosted.";
 		Fact[3] = "The best letter of the alphabet is objectively H."
 		Fact[4] = "This site had had a total of 3 - 5 extended downtimes in the ~1 year it's been active."
 		Fact[5] = "The web server this site runs on runs Fedora Linux."
-		Fact[6] = "The backend of this site has a massive spaghetti code problem."
+		Fact[6] = "The backend of this site has a massive spaghetti code problem. It works, at least."
+		Fact[7] = "Mitochondria is the powerhouse of the cell."
 
 		return Fact[jNum];
 	}
 
-document.write(getFact(randInt(6)));
+document.write(getFact(randInt(7)));
