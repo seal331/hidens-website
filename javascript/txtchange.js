2ccb7b5c2f18d4c19695e04a73393f77429d02a8
pinksub <114634497+pinksub@users.noreply.github.com>
2022-10-20 15:10:43 -0400

    I'm dumb

--- javascript/txtchange.js
+++ javascript/txtchange.js
@@ -1,11 +1,14 @@
 var txt = ['WHAT H', 'Foo', 'Bar', 'h', 'H', 'Test-Text-For-A-Test-Thing', 'gentoo balls'];
 var i = 0;
 
-setTimeout(function() {
-    if (txt.length < i) {
-        document.getElementById("sequence").innerHTML = txt[i];
+document.getElementById("sequence").innerHTML = txt[i];
+        
+setInterval(function() {
+    if (i < txt.length) {
         i++;
     } else {
         i = 0;
     }
+    
+    document.getElementById("sequence").innerHTML = txt[i];
 }, 1000);
