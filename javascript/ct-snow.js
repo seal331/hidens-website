2a1996f53d15adc2320e8d563c28e71b6fd311b4
HIDEN64 <hiden64@protonmail.com>
2022-12-11 06:07:05 -0600

    tone down the amount of snow a tad

--- javascript/ct-snow.js
+++ javascript/ct-snow.js
@@ -7,7 +7,7 @@
 //Configure below to change URL path to the snow image
 var snowsrc = "/./././static/misc/snow.gif"
 // Configure below to change number of snow to render
-var no = 30;
+var no = 25;
 // Configure whether snow should disappear after x seconds (0=never):
 var hidesnowtime = 0;
 // Configure how much snow should drop down before fading ("windowheight" or "pageheight")
