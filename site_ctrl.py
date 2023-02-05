c795a7a993f33888b8f1b1058f3e9d58069d3ee6
HIDEN64 <hiden64@protonmail.com>
2023-02-05 11:37:07 -0600

    a big h

--- site_ctrl.py
+++ site_ctrl.py
@@ -30,6 +30,7 @@ def RunServ(serve_static = False, serve_storage = False, serve_js = False):
 	app.router.add_get("/computers/desktops/a5ke4/old/2023-01-16", page_comptuers_desktops_a5ke4_20230116)
 	app.router.add_get("/computers/desktops/a5ke4/old/2023-01-18", page_comptuers_desktops_a5ke4_20230118)
 	app.router.add_get("/computers/desktops/a5ke4/old/2023-01-25", page_comptuers_desktops_a5ke4_20230125)
+	app.router.add_get("/computers/desktops/a5ke4/old/2023-01-30", page_comptuers_desktops_a5ke4_20230130)
 	app.router.add_get("/computers/desktops/hpp23", page_computers_desktop_hpp23)
 	app.router.add_get('/blog', page_blog)
 	app.router.add_get('/blog/rss', blog_rss)
@@ -160,6 +161,11 @@ async def page_comptuers_desktops_a5ke4_20230118(req):
 async def page_comptuers_desktops_a5ke4_20230125(req):
 	return render(req, 'computers.desktops.a5ke4.2023-01-25.html', {
 		'title': '2013-01-25 Desktop | AORUS 5 KE4 Desktop | My computers'
+	})
+
+async def page_comptuers_desktops_a5ke4_20230130(req):
+	return render(req, 'computers.desktops.a5ke4.2023-01-30.html', {
+		'title': '2013-01-30 Desktop | AORUS 5 KE4 Desktop | My computers'
 	})
 
 async def page_computers_desktop_hpp23(req):
