2cc3f678caf001b71f5cac3ccaf273f312b8c507
HIDEN64 <hiden64@protonmail.com>
2023-03-28 20:29:16 -0500

    rewrite a thing

--- runserver.py
+++ runserver.py
@@ -6,37 +6,29 @@ def main():
 		
 	if sys.version_info < (3, 7):
 		raise Exception("Unsupported Python version. Please use Python 3.7 or later.")
+		
+	def create_json_file(filename, prompt_msg, success_msg):
+		if not os.path.isfile(filename):
+			resp = input(prompt_msg)
+			if resp == "" or resp.lower() == "y":
+				with open(filename, "w") as f:
+					f.write("[]")
+					print(success_msg)
+			elif resp.lower() == "n":
+				print(f"File not created. Things will break, beware!")
+			else:
+				print("Invalid option. Try again.")
 
-	gbjsonfile = "json/gb.json"
-	gbbansjsonfile = "json/gb_bans.json"
+	json_files = {
+		"json/gb.json": ("gb.json not found, would you like to create it? (Y/n): ", "File created successfully."),
+		"json/gb_bans.json": ("gb_bans.json not found, would you like to create it? (Y/n): ", "File created successfully.")
+	}
 
-	print("Checking Guestbook JSON file integrity...")
+	print("Checking JSON file integrity...")
 
-	# I need to call this multiple times because you can't add more than one positional argumenet to os.path.isfile, 
-	# TLDR: spaghetti code induced by Python bs
-	# I also could theoretically just use 'and', but I'm too lazy to rewrite this accordingly right now
-
-	if not os.path.isfile(gbjsonfile):
-		resp = input("gb.json not found, would you like to create it? (Y/n): ")
-		if resp == "" or resp == "y":
-			with open(gbjsonfile, "w") as f:
-				f.write("[]")
-				print("File created successfully.")
-		elif resp == "n":
-			print("File not created. The guestbook will not work unless you create the file!")
-		else:
-			print("Invalid option. Try again.")
-
-	if not os.path.isfile(gbbansjsonfile):
-		resp = input("gb_bans.json not found, would you like to create it? (Y/n): ")
-		if resp == "" or resp == "y":
-			with open(gbbansjsonfile, "w") as f:
-				f.write("[]")
-				print("File created successfully.")
-		elif resp == "n":
-			print("File not created. You will not be able to ban people from the Guestbook without this!")
-		else:
-			print("Invalid option. Try again.")
+	for filename, messages in json_files.items():
+		prompt_msg, success_msg = messages
+		create_json_file(filename, prompt_msg, success_msg)
 			
 	print("Check completed.")
 	print("Starting server...")
