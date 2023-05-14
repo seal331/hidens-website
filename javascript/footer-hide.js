ca6db4f6cd409b45a346b015cfc2590f8bfa1344
HIDEN64 <hiden64@protonmail.com>
2023-05-14 17:26:46 -0500

    yuge

--- javascript/footer-hide.js
+++ javascript/footer-hide.js
@@ -1,33 +1,31 @@
-// Get the footer element
 const footer = document.querySelector('footer');
 
-// Hide the footer if it's under an element with a higher z-index
-function hideFooter() {
-  if (!footer) return;
+function isFooterBelowHigherZIndexElements() {
+  if (!footer) return false;
   let footerTop = footer.getBoundingClientRect().top;
   let higherZIndexElements = document.querySelectorAll('*');
   higherZIndexElements = Array.prototype.filter.call(higherZIndexElements, function(element) {
     return window.getComputedStyle(element).getPropertyValue('z-index') > 0;
   });
-  let shouldHideFooter = false;
   for (let i = 0; i < higherZIndexElements.length; i++) {
     let elementBottom = higherZIndexElements[i].getBoundingClientRect().bottom;
     if (elementBottom > footerTop) {
-      shouldHideFooter = true;
-      break;
+      return true;
     }
   }
-  if (shouldHideFooter) {
-    if (footer.style.display !== 'none') {
-      footer.style.display = 'none';
-    }
+  return false;
+}
+
+function stickFooterToBottom() {
+  if (!footer) return;
+  if (isFooterBelowHigherZIndexElements()) {
+    footer.style.bottom = '100%'
+    footer.style.position = 'relative';
+    footer.style.marginLeft = '0%'
+    footer.style.width = '95%'
   } else {
-    if (footer.style.display !== 'block') {
-      footer.style.display = 'block';
-    }
+    footer.style.position = 'fixed';
   }
 }
 
-// Call the hideFooter function whenever the window is resized or scrolled
-window.addEventListener('resize', hideFooter);
-window.addEventListener('scroll', hideFooter);
+window.addEventListener('scroll', stickFooterToBottom);
