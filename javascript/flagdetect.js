5517cfbf806ee011458aaf6cc4413550c13c846d
HIDEN64 <hiden64@protonmail.com>
2023-06-03 19:02:26 -0500

    indention consistency

--- javascript/flagdetect.js
+++ javascript/flagdetect.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	var flagcounternotice = document.getElementById("flagcounternotice");
   
 	image.addEventListener('error', function() {
-	  flagcounternotice.style.display = "block";
+		flagcounternotice.style.display = "block";
 	});
   
 	image.addEventListener('load', function() {
-	  flagcounternotice.style.display = "none";
+		flagcounternotice.style.display = "none";
 	});
   
 	if (!image.complete) {
-	  flagcounternotice.style.display = "none";
+		flagcounternotice.style.display = "none";
 	}
-  });
+});
   
